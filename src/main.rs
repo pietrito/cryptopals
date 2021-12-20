0d2cee4458c90642492a3568142e519c03a092c4
pietrito <fanikulapo@proton.me>
2021-12-20 22:42:51 +0100

    Middle of Set 1 chall 6

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 mod set1;
 
-fn main() {
-    println!("Hello, world!");
+use crate::set1::*;
 
-    // let set1_chall1 = challenge1(&"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string());
-    // println!("Answer of Set1 challenge1: {}", set1_chall1);
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
-    // let set1_chall2 = set1::challenge2(
-    // &"1c0111001f010100061a024b53535009181c".to_string(),
-    // &"686974207468652062756c6c277320657965".to_string(),
-    // );
+fn main() -> Result<()> {
+    let set1_chall1 = challenge1(&"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string())?;
+    println!("Answer of Set1 challenge1: {}", set1_chall1);
+
+    let set1_chall2 = challenge2(
+        &"1c0111001f010100061a024b53535009181c".to_string(),
+        &"686974207468652062756c6c277320657965".to_string()
+    )?;
+    println!("Answer of Set1 challenge2: {}", set1_chall2);
+
+    let set1_chall3 = challenge3("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")?;
+    println!("Answer of Set1 challenge3: {}", set1_chall3);
+
+    let (line_number, line, key) = challenge4("data/set_1_challenge_4.txt")?;
+    println!("Answer of Set1 challenge4: #{} - {} - Key: {}", line_number, line, key);
+    println!("Decoded of Set1 challenge4: {}", challenge3(&line)?);
+
+    let set1_chall5 = challenge5(&"Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal".as_bytes().to_vec(), &"ICE".as_bytes().to_vec())?;
+    println!("Answer of Set1 challenge5: {}", set1_chall5);
+
+    Ok(())
 }
--- src/set1.rs
+++ src/set1.rs
@@ -1,4 +1,9 @@
+use std::fs;
+
 extern crate base64;
+use std::str;
+use std::fs::File;
+use std::io::{prelude::*, BufReader};
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync + 'static>>;
 
@@ -27,6 +32,30 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test_challenge3() -> Result<()> {
+        let decrypted = challenge3("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")?;
+
+        println!("Decrypted: {}", decrypted);
+        Ok(())
+    }
+
+    #[test]
+    fn test_challenge5() -> Result<()> {
+        assert_eq!("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f", challenge5(&"Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal".as_bytes().to_vec(), &"ICE".as_bytes().to_vec())?);
+        Ok(())
+    }
+
+    #[test]
+    fn test_hamming_distance() -> Result<()> {
+        assert_eq!(hamming_distance(
+             "this is a test", "wokka wokka!!!")?, 37);
+
+        Ok(())
+    }
+
 }
 
 pub fn challenge1(input: &String) -> Result<String> {
@@ -34,18 +63,136 @@ pub fn challenge1(input: &String) -> Result<String> {
 }
 
 pub fn challenge2(left: &String, right: &String) -> Result<String> {
-    let in_left = hex::string_to_vec_u8(left)?;
-    let in_right = hex::string_to_vec_u8(right)?;
+    let left_bytes = hex::string_to_vec_u8(left)?;
+    let right_bytes = hex::string_to_vec_u8(right)?;
 
-    if in_left.len() != in_right.len() {
-        panic!("Not same length {} - {}", in_left.len(), in_right.len());
+    if right_bytes.len() != right_bytes.len() {
+        panic!("Not same length {} - {}", left_bytes.len(), right_bytes.len());
     }
 
-    let mut out_bytes: Vec<u8> = Vec::with_capacity(in_left.len());
+    let out_bytes = do_xor(left_bytes, right_bytes)?;
 
-    for i in 0..in_left.len() {
-        out_bytes.push(in_left[i] ^ in_right[i]);
+    hex::vec_u8_to_string(out_bytes)
+}
+
+fn do_xor(left: Vec<u8>, right: Vec<u8>) -> Result<Vec<u8>> {
+    if right.len() != right.len() {
+        panic!("Not same length {} - {}", left.len(), right.len());
     }
 
-    return hex::vec_u8_to_string(out_bytes);
+    let mut out_bytes: Vec<u8> = Vec::with_capacity(left.len());
+
+    for i in 0..left.len() {
+        out_bytes.push(left[i] ^ right[i]);
+    }
+
+    Ok(out_bytes)
+}
+
+pub fn do_single_xor(input: &Vec<u8>, k: u8) -> Result<Vec<u8>> {
+    Ok(input.iter().map(|b| *b ^ k).collect())
+}
+
+pub fn challenge3(input: &str) -> Result<String> {
+    // let dict = build_dict("data/pride_and_prejudice.txt")?;
+    let dict = build_dict("data/alice_wonderlands.txt")?;
+    let (key, _) = crack_single_xor(&hex::string_to_vec_u8(input)?, &dict)?;
+
+    /*
+        let bonus = "ETAOIN SHRDLU".as_bytes().to_vec();
+        let bonus_clear = do_single_xor(&bonus, key)?;
+        println!("Bonus of Set1 challenge3: {}", str::from_utf8(&bonus_clear)?);
+    */
+
+    Ok(String::from_utf8(do_single_xor(&hex::string_to_vec_u8(input)?, key)?)?)
+}
+
+fn crack_single_xor(input: &Vec<u8>, dict: &[f32; 256]) -> Result<(u8, f32)> {
+    let mut max_score: f32 = 0.0f32;
+    let mut key = 0u8;
+
+    for k in 0..=255 {
+        let xored = do_single_xor(input, k)?;
+        let score = xored.iter().map(|b| dict[*b as usize]).sum();
+
+        if score > max_score {
+            max_score = score;
+            key = k;
+        }
+    }
+
+    Ok((key, max_score))
+}
+
+fn build_dict(path: &str) -> Result<[f32; 256]> {
+    let file = fs::read_to_string(path)?;
+    let mut out_dict = [0.0f32; 256];
+
+    for c in file.bytes() {
+        out_dict[c as usize] += 1.0f32;
+    }
+
+    for e in &mut out_dict {
+        *e /= file.len() as f32;
+    }
+
+    Ok(out_dict)
+}
+
+pub fn challenge4(path: &str) -> Result<(usize, String, u8)> {
+    let mut max_score = 0.0f32;
+    let mut max_key = 0u8;
+    let mut xored_line_number = 0;
+    let mut xored_line = String::new();
+
+    let file = File::open(path)?;
+    let dict = build_dict("./data/alice_wonderlands.txt")?;
+    let reader = BufReader::new(file);
+
+    for (line_number, line) in reader.lines().enumerate() {
+        let as_bytes = hex::string_to_vec_u8(&line.as_ref().unwrap())?;
+        let (key, score) = crack_single_xor(&as_bytes, &dict)?;
+        if  score > max_score {
+            max_score = score;
+            xored_line = line.unwrap();
+            max_key = key;
+            xored_line_number = line_number;
+        }
+    }
+    
+    Ok((xored_line_number, xored_line, max_key))
+}
+
+pub fn challenge5(input: &Vec<u8>, key: &Vec<u8>) -> Result<String> {
+    Ok(hex::vec_u8_to_string(do_vigenere(input, key)?)?)
+}
+
+fn do_vigenere(input: &Vec<u8>, key: &Vec<u8>) -> Result<Vec<u8>> {
+    let mut out: Vec<u8> = Vec::with_capacity(input.len());
+
+    for (i, b) in input.bytes().enumerate() {
+        out.push(input[i] ^ key[i%key.len()]);
+    }
+
+    Ok(out)
+}
+
+fn crack_vigenere(path: &str) -> Result<String> {
+
+
+    Ok(String::new())
+}
+
+fn guess_key_size(input: Vec<u8>) -> Result<u32> {
+    for key_size in 2..40u32 {
+        let left = input[0..key_size];
+    }
+}
+
+fn hamming_distance(left: &str, right: &str) -> Result<u32> {
+    if left.len() != right.len() {
+        panic!("Not same length: {} - {}", left.len(), right.len())
+    }
+
+    Ok((0..left.len()).map(|i| (left.as_bytes()[i] ^ right.as_bytes()[i]).count_ones()).sum())
 }
